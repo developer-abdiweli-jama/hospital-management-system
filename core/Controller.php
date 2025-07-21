59a6c6f5cb581f0350699c0ac4fcd57d02abb51a
Abdiweli Jama <abdiwelijamac@gmail.com>
2025-07-21 15:26:10 +0300

    Commit the new version

--- core/Controller.php
+++ core/Controller.php
@@ -2,7 +2,7 @@
 namespace App\Core;
 
 class Controller {
-    protected function view($view, $data = []) {
+    protected function render($view, $data = []) {
         extract($data);
         require_once VIEW_PATH . $view . '.php';
     }
@@ -10,6 +10,13 @@ class Controller {
     protected function redirect($url) {
         header('Location: ' . BASE_URL . $url);
         exit;
+    }
+
+    protected function generateCsrfToken() {
+        if (!isset($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
     }
 
     protected function validateCsrfToken($token) {
